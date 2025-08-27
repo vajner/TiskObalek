dc900f6ad547bb4c13f8df3509bf4f0d84b5376d
vajner <vajner@seznam.cz>
2025-08-27 21:35:30 +0200

    Nova

---
+++ neco/__init__.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+from tkinter import ttk
+
+#okno
+prihlasovaciOkno = tk.Tk()
+prihlasovaciOkno.geometry('400x200')
+prihlasovaciOkno.title('Přihlášení')
+
+
+
+#run
+prihlasovaciOkno.mainloop()
